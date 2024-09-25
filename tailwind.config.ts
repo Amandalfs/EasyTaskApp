4b42188f4dd5e20527e1ea2eb1a6c1e0c71662c2
Amandalfs <amandalfs57@gmail.com>
2024-09-25 08:17:07 -0300

    chore: configurado a fonte e cores do projeto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'var(--font-inter)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primaryColors: {
+          1: "#4D0084",
+          2: "#8E00F4",
+          3: "#B431FF",
+          4: "#DDA3FF",
+        },
+        neutral: {
+          1: "#0D0D0D",
+          2: "#3C3C3C",
+          3: "#7A7A7A",
+          4: "#BEBEBE"
+        }
       },
     },
   },
