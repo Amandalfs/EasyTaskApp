d21dce90d58dcd61abdaccdd0fad07a317b1f215
Amandalfs <amandalfs57@gmail.com>
2024-09-26 12:09:47 -0300

    feat: adicionado semantica na pagina inicial do projeto e adicionado a logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,34 @@
 import Image from 'next/image'
 import Robot from "@/assets/robot.png";
+import Logo from "@/assets/logo.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen mx-8 gap-8">
-      <div className="flex">
-        <div className="flex-row">
+    <section className="flex flex-col justify-center items-center h-screen mx-8 gap-8">
+        <header className="flex flex-row gap-1">
+          <Image      
+            src={Logo}
+            alt="Task Easy" 
+          />
           <h1 className="font-sans font-semibold text-2xl text-primaryColors-2">TaskEasy</h1>
-        </div>
-      </div>
+        </header>
 
-      <div className="flex flex-col gap-5  items-center mx-30">
-        <div className="flex flex-col gap-5">
+      <section className="flex flex-col gap-5  items-center m-8">
+        <article className="flex flex-col gap-5">
           <h1 className="font-sans text-[24px] font-semibold leading-[28.8px] text-left text-primaryColors-3">Boas-vindas!</h1>
           <p className="font-sans text-[16px] font-normal leading-[22.4px] text-left text-primaryColors-1">Precisando de uma ferramenta pra te acompanhar em todos os aspectos da sua vida?</p>
-        </div>
+        </article>
 
         <Image      
         src={Robot}
         width={260}
         height={178}
         alt="Robot" />
-      </div>
+      </section>
 
-      <div className='flex flex-col gap-2'>
+      <footer className='flex flex-col gap-2'>
         <button className='rounded-lg
-        bg-primaryColors-1 
+        bg-primaryColors-1
           font-sans text-[16px]
           font-semibold 
           leading-[22.4px] 
@@ -50,7 +53,7 @@ export default function Home() {
           border-2
           border-[#8003D3] 
         '>Já tenho uma conta</button>
-      </div>
-    </div>
+      </footer>
+    </section>
   );
 }
